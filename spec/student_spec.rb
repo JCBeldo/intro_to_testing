bd55fb0ae77cbb6cc10cd4277b91a3bcd853e658
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-01 17:45:17 -0500

    Give cookies to students.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,13 +1,40 @@
-# Add the 2 requires you will need here
+require 'rspec'
+require './lib/student'
 
-
-describe Student do
+RSpec.describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
-      # write the code to initialize a new student object
+      student = Student.new('Penelope')
+
+      # expect(student).to be_instance_of(Student)
+      expect(student).to be_a(Student)
     end
-    # test it has a name
-    # test it has cookies
-    # test it can add cookies
+  
+    it 'has name' do
+      student = Student.new('Penelope')
+      student_2 = Student.new('Bill')
+
+      expect(student.name).to eq('Penelope')
+      expect(student_2.name).to eq('Bill')
+    end
+    require 'pry'; binding.pry
+
+    it 'has cookies' do
+      student = Student.new('Penelope')
+      student_2 = Student.new('Bill')
+
+      expect(student.cookies).to eq([])
+      expect(student_2.cookies).to eq([])
+    end
+  end
+
+  describe '#add_cookie' do
+    it 'can add cookie' do
+      student = Student.new('Penelope')
+
+      student.add_cookie(['Chocolate Chunk'])
+      student.add_cookie(['Snickerdoodle'])
+    end
   end
 end
+# require 'pry'; binding.pry
